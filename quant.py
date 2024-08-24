2e9bed1006d7d28e84ab0d219d6a57f96ea587cb
0xgleb <gleb.dianov@protonmail.com>
2024-08-24 17:58:55 +0200

    get best/worst 30d performers from dydx

--- quant.py
+++ quant.py
@@ -1,6 +1,7 @@
 import asyncio
 from dydx_v4_client.indexer.rest.indexer_client import IndexerClient
 from dydx_v4_client.network import make_mainnet
+import pandas as pd
 
 NODE_URL = "https://dydx-rpc.publicnode.com:443"
 INDEXER_REST_URL = "https://indexer.dydx.trade"
@@ -20,24 +21,71 @@ async def setup_client():
 client = asyncio.run(setup_client())
 
 
-async def get_all_markets():
+async def get_all_markets() -> pd.DataFrame:
     response = await client.markets.get_perpetual_markets()
-    print(response)
+    markets = [response["markets"][ticker] for ticker in response["markets"]]
 
+    df = pd.DataFrame(markets)
+    df = df[df["status"] == "ACTIVE"]
+    df = df[df["marketType"] == "CROSS"]
+    df["volume24H"] = df["volume24H"].astype(float)
+    df = df[df["volume24H"] > 1000]
+    df = df[df["trades24H"] > 10]
 
-asyncio.run(get_all_markets())
-
-
-async def get_candles():
-    response = await client.markets.get_perpetual_market_candles(
-        market="BTC-USD", resolution="1MIN"
+    df = df.drop(
+        columns=["status", "marketType", "openInterestLowerCap", "openInterestUpperCap"]
     )
-    candles = response["candles"]
-    if candles:
-        latest_candle = candles[0]
-        print(
-            f"Latest candle: Open {latest_candle['open']}, Close {latest_candle['close']}, High {latest_candle['high']}, Low {latest_candle['low']}"
+
+    print(df.info())
+    return df
+
+
+async def get_candles(market: str) -> pd.DataFrame:
+    response = await client.markets.get_perpetual_market_candles(
+        market=market, resolution="1HOUR"
+    )
+    candles = pd.DataFrame(response["candles"]).drop(columns=["ticker", "resolution"])
+    candles["close"] = candles["close"].astype(float)
+    candles["open"] = candles["open"].astype(float)
+    candles.sort_values("startedAt", inplace=True)
+
+    return candles
+
+
+async def get_markets_candles() -> pd.DataFrame:
+    markets = await get_all_markets()
+
+    for market in markets.iterrows():
+        ticker = market[1]["ticker"]
+        candles = await get_candles(ticker)
+
+        markets.loc[market[0], "diff_24h"] = (
+            1 - candles["close"].iloc[-1] / candles["open"].iloc[-24]
+        )
+        markets.loc[market[0], "diff_7d"] = (
+            1 - candles["close"].iloc[-1] / candles["open"].iloc[-7 * 24]
         )
 
+        if candles.shape[0] < 30 * 24:
+            continue
 
-asyncio.run(get_candles())
+        markets.loc[market[0], "diff_30d"] = (
+            1 - candles["close"].iloc[-1] / candles["open"].iloc[-30 * 24]
+        )
+
+        print(markets.loc[market[0]], "\n")
+
+    return markets
+
+
+markets = asyncio.run(get_markets_candles())
+markets.to_csv("markets.csv", index=False)
+
+csv = "markets.csv"
+df = pd.read_csv(csv)
+
+df.info()
+
+df.sort_values("diff_30d", ascending=False, inplace=True)
+print(df.head())
+print(df.tail())

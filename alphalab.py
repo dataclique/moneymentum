10705ed9a445d7d39c62d7f79799569e7e979042
0xgleb <gleb.dianov@protonmail.com>
2024-09-03 14:00:00 +0200

    relative returns

--- alphalab.py
+++ alphalab.py
@@ -1,73 +1,122 @@
-from asyncio import run
-from dash import Dash, html, dcc, dash_table
-import plotly.express as px
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
 
-from perpy.dydx import get_candles, get_all_markets
-
-tickers = run(get_all_markets())
-df = run(get_candles(tickers))
-
-vol_df = df.pivot(columns="ticker", values="volume_usd").dropna()
-vol_df["market"] = vol_df.sum(axis=1)
-
-vol_ratio_df = vol_df.div(vol_df["market"], axis=0)
-
-returns_df = df.pivot(columns="ticker", values="return").dropna()
-returns_df["market"] = returns_df.mul(vol_ratio_df).sum(axis=1)
-
-cum_returns_df = returns_df.cumsum()
+from perpy.dydx import prep_candles
 
 external_stylesheets = [dbc.themes.CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+# tickers = run(get_all_markets())
+# df = run(get_candles(tickers))
+df = prep_candles(pd.read_csv("./data/candles.csv"))
+tickers = df["ticker"].unique()
+
+volume_df = df.pivot(columns="ticker", values="volume_usd").dropna()
+
+market_df = pd.DataFrame(index=volume_df.index)
+market_df["volume"] = volume_df.sum(axis=1)
+
+volume_ratio_df = volume_df.div(market_df["volume"], axis=0)
+
+returns_df = df.pivot(columns="ticker", values="return").dropna()
+returns_df["market"] = returns_df.mul(volume_ratio_df).sum(axis=1)
+
+cum_returns_df = returns_df.cumsum()
+
+market_df["return"] = returns_df.mul(volume_ratio_df).sum(axis=1)
+market_df["cum_return"] = market_df["return"].cumsum()
+
+rel_returns_df = returns_df.sub(market_df["return"], axis=0)
 
 width = 6
 app.layout = dbc.Container(
     [
         dbc.Row([html.H1(children="dYdX", style={"textAlign": "center"})]),
         html.Hr(),
+        dcc.Graph(
+            figure={
+                "data": [
+                    go.Scatter(
+                        x=rel_returns_df.index,
+                        y=rel_returns_df[col],
+                        mode="lines",
+                        name=col,
+                    )
+                    for col in rel_returns_df.columns
+                ],
+                "layout": go.Layout(
+                    title="Cumulative Returns Over Time",
+                    xaxis={
+                        "title": "Date",
+                        "rangeslider": {"visible": True},
+                    },
+                    yaxis={"title": "Cumulative Return"},
+                ),
+            }
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    go.Scatter(
+                        x=cum_returns_df.index,
+                        y=cum_returns_df[col],
+                        mode="lines",
+                        name=col,
+                    )
+                    for col in cum_returns_df.columns
+                ],
+                "layout": go.Layout(
+                    title="Cumulative Returns Over Time",
+                    xaxis={"title": "Date"},
+                    yaxis={"title": "Cumulative Return"},
+                ),
+            }
+        ),
         dbc.Row(
             [
                 dbc.Col(
                     [
                         dcc.Graph(
-                            figure=px.area(cum_returns_df, y="market"),
+                            figure=px.area(market_df, y="cum_return"),
                         ),
                     ],
                     width=width,
                 ),
                 dbc.Col(
                     [
-                        dash_table.DataTable(
-                            data=cum_returns_df.to_dict("records"),
-                            page_size=10,
-                            style_table={"overflowX": "auto"},
-                        ),
+                        dcc.Graph(figure=px.area(market_df, y="volume")),
                     ],
                     width=width,
                 ),
             ]
         ),
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        dcc.Graph(figure=px.area(vol_df, y="market")),
-                    ],
-                    width=width,
-                ),
-                dbc.Col(
-                    [
-                        dash_table.DataTable(
-                            data=vol_df.to_dict("records"),
-                            page_size=10,
-                            style_table={"overflowX": "auto"},
-                        ),
-                    ],
-                    width=width,
-                ),
-            ]
-        ),
+        # dbc.Row(
+        #     [
+        #         dbc.Col(
+        #             [
+        # dash_table.DataTable(
+        #     data=cum_returns_df.to_dict("records"),
+        #     page_size=10,
+        #     style_table={"overflowX": "auto"},
+        # ),
+        #             ],
+        #             width=width,
+        #         ),
+        #         dbc.Col(
+        #             [
+        #                 dash_table.DataTable(
+        #                     data=volume_df.to_dict("records"),
+        #                     page_size=10,
+        #                     style_table={"overflowX": "auto"},
+        #                 ),
+        #             ],
+        #             width=width,
+        #         ),
+        #     ]
+        # ),
     ],
     fluid=True,
 )

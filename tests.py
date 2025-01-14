e6e3c9e3bb3ebd13809a1f86fe15a327373de1e3
levsgit <shatorv.lev@gmail.com>
2025-01-14 16:06:48 +0400

    fix: strict equality + remove test from pipeline

--- tests.py
+++ tests.py
@@ -5,7 +5,6 @@ from pyspark.sql import functions as F
 # Assuming Chronos, SchemaOHLCV, and the logger are imported or defined elsewhere
 from pipeline import Chronos, SchemaOHLCV, logger
 
-
 @pytest.fixture(scope="module")
 def spark_session():
     """Fixture to initialize a Spark session."""
@@ -39,23 +38,15 @@ def test_aave_last_record(spark_session):
         .collect()[0]
     )
     # Google sheet: 0.29%
-    assert aave_last_record["mean_return"] == pytest.approx(
-        0.0029290822331748565
-    ), "Mean return mismatch"
+    assert aave_last_record["mean_return"] == 0.0029290822331748565, "Mean return mismatch"
     # Google sheet: 0.05
-    assert aave_last_record["return_stddev"] == pytest.approx(
-        0.05116662705011916
-    ), "Return stddev mismatch"
+    assert aave_last_record["return_stddev"] == 0.05116662705011916, "Return stddev mismatch"
     # Google sheet: 97.76%
-    assert aave_last_record["annualized_volatility"] == pytest.approx(
-        0.9775370372243625
-    ), "Annualized volatility mismatch"
+    assert aave_last_record["annualized_volatility"] == 0.9775370372243625, "Annualized volatility mismatch"
     # Google sheet: 0.29
-    assert aave_last_record["beta"] == pytest.approx(0.29206334291840363), "Beta mismatch"
+    assert aave_last_record["beta"] == 0.29206334291840363, "Beta mismatch"
     # Google sheet: 7.67E-04
-    assert aave_last_record["covariance"] == pytest.approx(
-        7.646287605794159e-4
-    ), "Covariance mismatch"
+    assert aave_last_record["covariance"] == 7.646287605794159e-4, "Covariance mismatch"
 
     logger.info("AAVE last record assertions passed.")
 
@@ -86,23 +77,15 @@ def test_btc_last_record(spark_session):
     )
 
     # Google sheet: 0.22%
-    assert btc_last_record["mean_return"] == pytest.approx(
-        0.002159416213018761
-    ), "Mean return mismatch"
+    assert btc_last_record["mean_return"] == 0.002159416213018761, "Mean return mismatch"
     # Google sheet: 0.03
-    assert btc_last_record["return_stddev"] == pytest.approx(
-        0.027437960505078306
-    ), "Return stddev mismatch"
+    assert btc_last_record["return_stddev"] == 0.027437960505078306, "Return stddev mismatch"
     # Google sheet: 52.42%
-    assert btc_last_record["annualized_volatility"] == pytest.approx(
-        0.5242014994136859
-    ), "Annualized volatility mismatch"
+    assert btc_last_record["annualized_volatility"] == 0.5242014994136859, "Annualized volatility mismatch"
     # Google sheet: 1.00
-    assert btc_last_record["beta"] == pytest.approx(0.9972899728997291), "Beta mismatch"
+    assert btc_last_record["beta"] == 0.9972899728997291, "Beta mismatch"
     # Google sheet: 7.53E-04
-    assert btc_last_record["covariance"] == pytest.approx(
-        7.508014553322255e-4
-    ), "Covariance mismatch"
+    assert btc_last_record["covariance"] == 7.508014553322255e-4, "Covariance mismatch"
 
     logger.info("BTC last record assertions passed.")
 
@@ -133,22 +116,14 @@ def test_ai_last_record(spark_session):
     )
 
     # Google sheet: -1.27%
-    assert ai_last_record["mean_return"] == pytest.approx(
-        -0.012734350383113274
-    ), "Mean return mismatch"
+    assert ai_last_record["mean_return"] == -0.012734350383113274, "Mean return mismatch"
     # Google sheet: 0.16
-    assert ai_last_record["return_stddev"] == pytest.approx(
-        0.16124563962681307
-    ), "Return stddev mismatch"
+    assert ai_last_record["return_stddev"] == 0.16124563962681307, "Return stddev mismatch"
     # Google sheet: 116.28%
-    assert ai_last_record["annualized_volatility"] == pytest.approx(
-        1.1627588432389253
-    ), "Annualized volatility mismatch"
+    assert ai_last_record["annualized_volatility"] == 1.1627588432389253, "Annualized volatility mismatch"
     # Google sheet: 0.28
-    assert ai_last_record["beta"] == pytest.approx(0.27410619693561644), "Beta mismatch"
+    assert ai_last_record["beta"] == 0.27410619693561644, "Beta mismatch"
     # Google sheet: 7.27E-03
-    assert ai_last_record["covariance"] == pytest.approx(
-        0.007126803962757325
-    ), "Covariance mismatch"
+    assert ai_last_record["covariance"] == 0.007126803962757325, "Covariance mismatch"
 
     logger.info("AI last record assertions passed.")

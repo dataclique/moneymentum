632ad84d76710a16d0b408c74742e14660add84a
levsgit <shatrov.lev@mail.ru>
2025-07-14 21:44:13 +0400

    fix: formatting

--- tests/test_chronos.py
+++ tests/test_chronos.py
@@ -51,9 +51,9 @@ def test_aave_last_record(spark_session):
     # Google sheet: 0.05
     assert aave_last_record["return_stddev"] == 0.05116662705011916, "Return stddev mismatch"
     # Google sheet: 97.76%
-    assert (
-        aave_last_record["annualized_volatility"] == 0.9775370372243625
-    ), "Annualized volatility mismatch"
+    assert aave_last_record["annualized_volatility"] == 0.9775370372243625, (
+        "Annualized volatility mismatch"
+    )
     # Google sheet: 0.29
     assert aave_last_record["beta"] == 0.29206334291840363, "Beta mismatch"
     # Google sheet: 7.67E-04
@@ -102,9 +102,9 @@ def test_btc_last_record(spark_session):
     # Google sheet: 0.03
     assert btc_last_record["return_stddev"] == 0.027437960505078306, "Return stddev mismatch"
     # Google sheet: 52.42%
-    assert (
-        btc_last_record["annualized_volatility"] == 0.5242014994136859
-    ), "Annualized volatility mismatch"
+    assert btc_last_record["annualized_volatility"] == 0.5242014994136859, (
+        "Annualized volatility mismatch"
+    )
     # Google sheet: 1.00
     assert btc_last_record["beta"] == 0.9972899728997291, "Beta mismatch"
     # Google sheet: 7.53E-04
@@ -153,9 +153,9 @@ def test_ai_last_record(spark_session):
     # Google sheet: 0.16
     assert ai_last_record["return_stddev"] == 0.16124563962681307, "Return stddev mismatch"
     # Google sheet: 116.28%
-    assert (
-        ai_last_record["annualized_volatility"] == 1.1627588432389253
-    ), "Annualized volatility mismatch"
+    assert ai_last_record["annualized_volatility"] == 1.1627588432389253, (
+        "Annualized volatility mismatch"
+    )
     # Google sheet: 0.28
     assert ai_last_record["beta"] == 0.27410619693561644, "Beta mismatch"
     # Google sheet: 7.27E-03

cb04140076bcf64252dd39c965f303d6736e665c
levsgit <shatrov.lev@mail.ru>
2025-07-14 21:01:12 +0400

    fix: formatting

--- backtest.py
+++ backtest.py
@@ -46,7 +46,7 @@ class BacktestPipeline:
 
         chronos = Chronos(timeframe=self.timeframe, config=self.config)
         analysis_df = self.strategy.generate_analysis(candles_df)
-        
+
         util.save_csv("analysis_df", analysis_df)
 
         picks_df = self.strategy.generate_picks(analysis_df)

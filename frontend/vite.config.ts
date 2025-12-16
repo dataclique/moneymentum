1478ddb284e4606500ce88a6aaa0979246ebb919
0xgleb <0xgleb@users.noreply.github.com>
2025-12-16 22:02:38 +0000

    fix dev build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -44,12 +44,7 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 6000,
     rollupOptions: {
-      external: [
-        "socks-proxy-agent",
-        "protobufjs/minimal.js",
-        "protobufjs/minimal",
-        "protobufjs",
-      ],
+      external: ["socks-proxy-agent"],
     },
   },
   server: {

f946874d71dda9129461809e92fe7d5ed2bea2f3
0xgleb <gleb.dianov@protonmail.com>
2026-02-18 01:32:33 +0400

    add lev

--- keys.nix
+++ keys.nix
@@ -1,16 +1,18 @@
 rec {
   keys = {
-    operator =
+    gleb =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHepyxN9hvXzbCY/z0amzldy7DXjNdyetnVaQexRgDEX";
+    lev =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLtz1pwdaRGeiebJ3qaPj+Xs1wtrTVqxRKlL/cRnCFy";
     ci =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHww2/XOFuvQONtwbJF5SFWtKazncH82P7iUmWw1duc6";
     host =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKt/inM5lJz+CjhIkNEn5p7Ojb1uyshlZ57UfGRSJwIj root@moneymentum-nixos";
   };
 
-  roles = {
-    infra = [ keys.operator keys.ci ];
-    service = [ keys.operator ];
-    ssh = [ keys.operator keys.ci ];
+  roles = with keys; {
+    infra = [ gleb lev ];
+    service = [ gleb lev ];
+    ssh = [ ci gleb lev ];
   };
 }
